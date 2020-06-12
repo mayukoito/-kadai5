56d7a76f6dbd9669629c5472012da43b88ab6471
yoshinosatsuki <yoshinosatsuki@192.168.10.6>
2020-06-12 15:33:45 +0900

    pair1回目

---
+++ kadai5/src/kadai5/IntToEng.java
@@ -0,0 +1,38 @@
+package kadai5;
+import java.util.Scanner;
+
+public class IntToEng {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(translateEng(input));
+    }
+
+    // 数値を英訳する変換するメソッド
+    static String translateEng(int n) {
+    	
+    	if(n==0) return "zero";
+    	if(n==1) return "one";
+    	if(n==2) return "two";
+    	if(n==3) return "three";
+    	if(n==4) return "four";
+    	if(n==5) return "five";
+    	if(n==6) return "six";
+    	if(n==7) return "seven";
+    	if(n==8) return "eight";
+    	if(n==9) return "nine";
+    	if(n==10) return "ten";
+    	if(n==11) return "eleven";
+    	if(n==12) return "twelve";
+    	if(n==13) return "thirteen";
+    	if(n==14) return "fourteen";
+    	if(n==15) return "fifteen";
+    	if(n==16) return "sixteen";
+    	if(n==17) return "seventeen";
+    	if(n==18) return "eighteen";
+    	if(n==19) return "nineteen";
+    	else return "";
+    }
+
+
+}
